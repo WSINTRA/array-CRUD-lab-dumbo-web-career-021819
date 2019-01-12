9812b4279892c2c37508c0a165384902760ce0ae
William Robert Simpson <will.simpson85@gmail.com>
2019-01-12 02:03:45 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -8,7 +8,7 @@ end
 
 def add_element_to_end_of_array(array, element)
   array_of_sorts = array
-  array_of_sorts>>element
+  array_of_sorts << element
 end
 
 def add_element_to_start_of_array(array, element)
