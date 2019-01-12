d2619e8dcad70bd17c1e21d0e5cdec08fd7a181b
William Robert Simpson <will.simpson85@gmail.com>
2019-01-12 02:02:59 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,7 +3,7 @@ def create_an_empty_array
 end
 
 def create_an_array
-  array_of_sorts = ["sort1,sort2"]
+  array_of_sorts = ["sort1","sort2",3,4]
 end
 
 def add_element_to_end_of_array(array, element)
